eeabe645a03eba39c279213eaee5b2cbd147bbfa
Ihar Zhuk <Ihar.Zhuk@epolsoft.com>
2019-04-10 19:28:53 +0300

    Finish chapter12

--- chapter11/app/component.js
+++ chapter11/app/component.js
@@ -11,18 +11,45 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require("@angular/core");
 var repository_model_1 = require("./repository.model");
 var ProductComponent = (function () {
-    function ProductComponent() {
+    function ProductComponent(ref) {
         this.model = new repository_model_1.Model();
+        this.fontSizeWithUnits = "30px";
+        this.fontSizeWithoutUnits = "30";
+        window.appRef = ref;
+        window.model = this.model;
     }
-    ProductComponent.prototype.getClasses = function () {
-        return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+    ProductComponent.prototype.getProductByPosition = function (position) {
+        return this.model.getProducts()[position];
+    };
+    ProductComponent.prototype.getClassesByPosition = function (position) {
+        var product = this.getProductByPosition(position);
+        return "p-a-1 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    };
+    ProductComponent.prototype.getClasses = function (key) {
+        var product = this.model.getProduct(key);
+        return "p-a-1 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    };
+    ProductComponent.prototype.getClassMap = function (key) {
+        var product = this.model.getProduct(key);
+        return {
+            "text-xs-center bg-danger": product.name == "Kayak",
+            "bg-info": product.price < 50
+        };
+    };
+    ProductComponent.prototype.getStyles = function (key) {
+        var product = this.model.getProduct(key);
+        return {
+            fontSize: "30px",
+            "margin.px": 100,
+            color: product.price > 50 ? "red" : "green"
+        };
     };
     ProductComponent = __decorate([
         core_1.Component({
             selector: "app",
             templateUrl: "app/template.html"
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [core_1.ApplicationRef])
     ], ProductComponent);
     return ProductComponent;
 }());
--- chapter11/app/component.ts
+++ chapter11/app/component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, ApplicationRef } from "@angular/core";
 import { Model } from "./repository.model";
+import { Product } from "./product.model";
 
 @Component({
     selector: "app",
@@ -8,7 +9,42 @@ import { Model } from "./repository.model";
 export class ProductComponent {
     model: Model = new Model();
 
-    getClasses(): string {
-        return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+    constructor(ref: ApplicationRef) {
+        (<any>window).appRef = ref;
+        (<any>window).model = this.model;
     }
+
+    getProductByPosition(position: number): Product {
+        return this.model.getProducts()[position];
+    }
+
+    getClassesByPosition(position: number): string {
+        let product = this.getProductByPosition(position);
+        return "p-a-1 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    }
+
+    getClasses(key: number): string {
+        let product = this.model.getProduct(key);
+        return "p-a-1 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    }
+
+    getClassMap(key: number): Object {
+        let product = this.model.getProduct(key);
+        return {
+            "text-xs-center bg-danger": product.name == "Kayak",
+            "bg-info": product.price < 50
+        };
+    }
+
+    getStyles(key: number) {
+        let product = this.model.getProduct(key);
+        return {
+            fontSize: "30px",
+            "margin.px": 100,
+            color: product.price > 50 ? "red" : "green"
+        };
+    }
+
+    fontSizeWithUnits: string = "30px";
+    fontSizeWithoutUnits: string = "30";
 }
